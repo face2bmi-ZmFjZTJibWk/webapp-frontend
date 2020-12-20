86426329ad19248849980b5f95cd8593e54adb4c
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2020-12-20 13:41:42 +0530

    add api request proxy

---
+++ api/getbmi.js
@@ -0,0 +1,28 @@
+let multiparty = require('multiparty')
+var fs = require('fs');
+var request = require('request');
+
+// Request Fowarder to Actual API
+module.exports = (req, res) => {
+    if (req.method === "POST") {
+        let form = new multiparty.Form();
+        // get image from POSTed Form
+        form.parse(req, (err, fields, files) => {
+            BACKEND_URL = process.env.APIURL
+
+            let image = files['file'][0] //our image
+
+            // POST the image to actual API sever
+            var requ = request.post(BACKEND_URL, function (err, resp, bodyy) {
+                // return the response as response to client
+                res.status(resp.statusCode).send(resp.body)
+            });
+            var formdd = requ.form();
+            formdd.append('file', fs.createReadStream(image.path));
+        });
+        return;
+    } else {
+        res.status(405).send("Method not allowed");
+        return;
+    }
+}
