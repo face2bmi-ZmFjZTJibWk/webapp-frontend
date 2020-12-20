548e6a2bb0c5400933f197bacc2267dadf358b28
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2020-12-20 17:23:15 +0530

    Revert "create image upload component"
    This reverts commit a0b6f6c3b6b4d8d7c6e3ca3d39dbe11dc9eed47a.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 import { HomeComponent, InfoDialogComponent } from './components/home/home.component';
-import { ImageUploadComponent } from './components/image-upload/image-upload.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ImageUploadComponent,
     InfoDialogComponent
   ],
   imports: [
