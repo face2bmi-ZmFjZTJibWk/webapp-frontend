3f9db62263946724e1928a09f531422c61f095f6
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2020-12-20 22:51:00 +0530

    use specific env vars for prod

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  BACKEND_URL = "/api/getbmi/"
+  BACKEND_URL = environment.apiUrl
 
   constructor(private http: HttpClient) { }
 
